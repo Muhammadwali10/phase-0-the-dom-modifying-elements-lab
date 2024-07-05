2652b7e633f849ae253664cc3a57f193b991c2ef
Muhammadwali10 <mohamedmowlid224@yahoo.com>
2024-07-05 12:05:01 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const elementToRemove = document.querySelector('main#main');
+if (elementToRemove) {
+    elementToRemove.remove();
+}
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = "Mohamed is the champion";
